0a0179a2303bdf0b8257f0d61bf577cfa9bfd20d
homesickAryan10 <rohillaaryan881@gmail.com>
2026-08-16 21:29:59 +0530

    Add files via upload

---
+++ day3_q5.c
@@ -0,0 +1,22 @@
+/* Q5: Write a program to convert temperature from Celsius to Fahrenheit.
+Sample Test Cases:
+Input 1:
+0
+Output 1:
+Fahrenheit=32
+
+Input 2:
+100
+Output 2:
+Fahrenheit=212
+*/
+#include <stdio.h>
+int main(){
+    int celsius, fahrenheit;
+    printf("Enter temprature in celsius : ");
+    scanf("%d", &celsius);
+    
+    fahrenheit = (celsius * 9 / 5) + 32;
+    printf("Temprature in fahrenheit = %d\n", fahrenheit);
+    return 0;
+}
